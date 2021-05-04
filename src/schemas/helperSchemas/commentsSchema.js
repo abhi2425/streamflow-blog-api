dda18f654cc41ee4614e8dd89d85225f8362d1fc
Abhinav_Jha <abhi.mount.26@gmail.com>
2021-05-04 20:46:09 +0530

    comment schema updated

--- src/schemas/helperSchemas/commentsSchema.js
+++ src/schemas/helperSchemas/commentsSchema.js
@@ -1,17 +1,18 @@
 const mongoose = require('mongoose')
 
 const commentsSchema = mongoose.Schema({
-   _id: false,
-   owner: {
-      type: String,
-      required: true,
-   },
-   description: String,
-   upVote: [String],
-   downVote: [String],
-   date: {
-      type: Date,
-      default: Date.now(),
-   },
+	_id: false,
+	owner: {
+		type: String,
+		required: true,
+	},
+	description: String,
+	upVote: [String],
+	ownerAvatar: String,
+	downVote: [String],
+	date: {
+		type: Date,
+		default: Date.now(),
+	},
 })
 module.exports = commentsSchema
