e30a6c81a912298e801f6f28bbc3ce379e8797fd
Abhinav_Jha <abhi.mount.26@gmail.com>
2021-05-05 01:56:34 +0530

    comment route fixed completely

--- src/routers/postsRouter/updatePosts.js
+++ src/routers/postsRouter/updatePosts.js
@@ -6,103 +6,109 @@ const UsersCollection = require('../../models/usersCollection')
 
 //update required field of a post---> only if you r owner of post
 router.patch(
-	'/profile/post/update/:title',
-	auth,
-	async ({ body, params, user }, res) => {
-		try {
-			const userUpdates = Object.keys(body)
-			const allowedUpdates = ['title', 'body', 'blogImages']
-			const isValidUpdate = userUpdates.every((updates) =>
-				allowedUpdates.includes(updates)
-			)
-			if (!isValidUpdate) {
-				return res.status(404).send({
-					error: `invalid property is not updated `,
-				})
-			}
-			const post = await PostsCollection.findOne({
-				title: params.title.toLowerCase(),
-				postOwner: user.userName.toLowerCase(),
-			})
-			//   const post1 = await PostsCollection.aggregate([
-			//      {
-			//         $match: {
-			//            title: params.title.toLowerCase(),
-			//            postOwner: user.userName.toLowerCase(),
-			//         },
-			//      },
-			//   ])
-			//   console.log(post1)
+  '/profile/post/update/:title',
+  auth,
+  async ({ body, params, user }, res) => {
+    try {
+      const userUpdates = Object.keys(body)
+      const allowedUpdates = ['title', 'body', 'blogImages']
+      const isValidUpdate = userUpdates.every((updates) =>
+        allowedUpdates.includes(updates)
+      )
+      if (!isValidUpdate) {
+        return res.status(404).send({
+          error: `invalid property is not updated `,
+        })
+      }
+      const post = await PostsCollection.findOne({
+        title: params.title.toLowerCase(),
+        postOwner: user.userName.toLowerCase(),
+      })
+      //   const post1 = await PostsCollection.aggregate([
+      //      {
+      //         $match: {
+      //            title: params.title.toLowerCase(),
+      //            postOwner: user.userName.toLowerCase(),
+      //         },
+      //      },
+      //   ])
+      //   console.log(post1)
 
-			// We can not use save method using aggregate and moreover it return an array of document so we have to go one step down to get the document.
+      // We can not use save method using aggregate and moreover it return an array of document so we have to go one step down to get the document.
 
-			if (!post) throw new Error('post not found!')
-			userUpdates.forEach((update) => (post[update] = body[update]))
-			await post.save()
-			res.status(202).send({ message: `${post.title}  updated successfully!!` })
-		} catch (error) {
-			res.status(400).send({ error: error.message })
-		}
-	}
+      if (!post) throw new Error('post not found!')
+      userUpdates.forEach((update) => (post[update] = body[update]))
+      await post.save()
+      res.status(202).send({ message: `${post.title}  updated successfully!!` })
+    } catch (error) {
+      res.status(400).send({ error: error.message })
+    }
+  }
 )
 // route for upVotes and downVotes any authorized person can access
 router.patch(
-	'/profile/post/votes/:title',
-	auth,
-	async ({ body, params }, res) => {
-		try {
-			const userUpdates = Object.keys(body)
-			const allowedUpdates = ['upVote', 'downVote']
-			const isValidUpdate = userUpdates.every((updates) =>
-				allowedUpdates.includes(updates)
-			)
-			if (!isValidUpdate) {
-				res.status(400).send({ error: error.message })
-			}
-			const post = await PostsCollection.findOne({
-				title: params.title.toLowerCase(),
-			})
-			if (!post) throw new Error('post not found!')
-			userUpdates.forEach((update) => (post[update] = body[update]))
-			await post.save()
-			res.status(202).send({ message: `${post.title}  updated successfully!!` })
-		} catch (error) {
-			res.status(400).send({ error: error.message })
-		}
-	}
+  '/profile/post/votes/:title',
+  auth,
+  async ({ body, params }, res) => {
+    try {
+      const userUpdates = Object.keys(body)
+      const allowedUpdates = ['upVote', 'downVote']
+      const isValidUpdate = userUpdates.every((updates) =>
+        allowedUpdates.includes(updates)
+      )
+      if (!isValidUpdate) {
+        res.status(400).send({ error: error.message })
+      }
+      const post = await PostsCollection.findOne({
+        title: params.title.toLowerCase(),
+      })
+      if (!post) throw new Error('post not found!')
+      userUpdates.forEach((update) => (post[update] = body[update]))
+      await post.save()
+      res.status(202).send({ message: `${post.title}  updated successfully!!` })
+    } catch (error) {
+      res.status(400).send({ error: error.message })
+    }
+  }
 )
 
 //add or update comments in a post
 router.patch(
-	'/profile/post/:userName/:postTitle/comment',
-	auth,
-	async ({ body, params, user }, res) => {
-		try {
-			const post = await PostsCollection.findOne({
-				postOwner: params.userName.toLowerCase(),
-				title: params.postTitle.toLowerCase(),
-			})
-			if (!post) throw new Error('post not found!!')
+  '/profile/post/:userName/:postTitle/comment',
+  auth,
+  async ({ body, params, user }, res) => {
+    try {
+      const post = await PostsCollection.findOne({
+        postOwner: params.userName.toLowerCase(),
+        title: params.postTitle.toLowerCase(),
+      })
+      if (!post) throw new Error('post not found!!')
 
-			const avatar = await UsersCollection.aggregate([
-				{
-					$match: {
-						userName: user.userName,
-					},
-				},
-				{ $project: { _id: 0, avatar: 1 } },
-			])
-
-			post.comments = [
-				{ ...body, owner: user.userName, ownerAvatar: avatar[0].avatar?.image },
-				...post.comments,
-			]
-			await post.save()
-			res.status(202).send({ message: 'comment posted!' })
-		} catch (error) {
-			res.status(404).send({ error: error.message })
-		}
-	}
+      const avatar = await UsersCollection.aggregate([
+        {
+          $match: {
+            userName: user.userName,
+          },
+        },
+        { $project: { _id: 0, avatar: 1 } },
+      ])
+      post.comments.forEach((comment) => {
+        if (comment.owner === user.userName) {
+          console.log(comment.ownerAvatar, avatar[0].avatar?.image)
+          comment.ownerAvatar = avatar[0].avatar?.image
+          console.log(comment.ownerAvatar, avatar[0].avatar?.image)
+        }
+      })
+      post.comments = [
+        { ...body, owner: user.userName, ownerAvatar: avatar[0].avatar?.image },
+        ...post.comments,
+      ]
+      await post.save()
+      res.status(202).send({ message: 'comment posted!', c: post.comments })
+    } catch (error) {
+      res.status(404).send({ error: error.message })
+    }
+  }
 )
 
 module.exports = router
